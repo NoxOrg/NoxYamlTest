abbc89fc6037d6531ac11544351333ac3e4ff2b3
Jan Schutte <JanSchutte.Lands@gmail.com>
2023-06-16 05:00:29 +0200

    Initial Commit

---
+++ NoxYamlTest/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Nox.Solution;
+
+var solution = new NoxSolutionBuilder()
+    .Build();
+
+Console.WriteLine($"Solution Name: {solution.Name}");
+Console.WriteLine($"Solution Description: {solution.Description}");
+Console.WriteLine();
+Console.WriteLine("Domain");
+foreach (var entity in solution.Domain!.Entities)
+{
+    Console.WriteLine($"Entity: {entity.Name} ({entity.Description})");    
+}
